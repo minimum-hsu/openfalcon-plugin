698d6977d9daeadb074b088ba97c064a010186ec
Minimum <s5550055@yahoo.com.tw>
2015-09-15 04:21:02 +0000

    Update.

--- sys/load/60_sys_loadavg.sh
+++ sys/load/60_sys_loadavg.sh
@@ -1,44 +1,38 @@
 #!/bin/bash
 
 # load average 1 min
-handle=$(cat /proc/loadavg | awk '{print $1}')
+handle1=$(cat /proc/loadavg | awk '{print $1}')
 date=`date +%s`
 host=$HOSTNAME
+
+# load average 5 min
+handle5=$(cat /proc/loadavg | awk '{print $2}')
+
+# load average 15 min
+handle15=$(cat /proc/loadavg | awk '{print $3}')
 
 echo "[{\
   \"endpoint\"   : \"$host\",\
   \"tags\"       : \"\",\
   \"timestamp\"  : $date,\
   \"metric\"     : \"sys.loadavg.1min\",\
-  \"value\"      : $handle,\
+  \"value\"      : $handle1,\
   \"counterType\": \"GUAGE\",\
-  \"step\"       : 60}]"
-
-# load average 5 min
-handle=$(cat /proc/loadavg | awk '{print $2}')
-date=`date +%s`
-host=$HOSTNAME
-
-echo "[{\
+  \"step\"       : 60},\
+  {\
   \"endpoint\"   : \"$host\",\
   \"tags\"       : \"\",\
   \"timestamp\"  : $date,\
   \"metric\"     : \"sys.loadavg.5min\",\
-  \"value\"      : $handle,\
+  \"value\"      : $handle5,\
   \"counterType\": \"GUAGE\",\
-  \"step\"       : 60}]"
-
-# load average 15 min
-handle=$(cat /proc/loadavg | awk '{print $3}')
-date=`date +%s`
-host=$HOSTNAME
-
-echo "[{\
+  \"step\"       : 60},\
+  {\
   \"endpoint\"   : \"$host\",\
   \"tags\"       : \"\",\
   \"timestamp\"  : $date,\
   \"metric\"     : \"sys.loadavg.15min\",\
-  \"value\"      : $handle,\
+  \"value\"      : $handle15,\
   \"counterType\": \"GUAGE\",\
   \"step\"       : 60}]"
 
